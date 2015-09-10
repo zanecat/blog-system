d59e5accbe0673e31621077aac6e552ba7bdc7b6
Ray <xraycbr@gmail.com>
2015-09-10 15:50:31 +1000

    user info verification

--- db/migrate/20150910044937_create_users.rb
+++ db/migrate/20150910044937_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
+	def change
+		create_table :users do |t|
+			t.string :name
+			t.string :email
 
-      t.timestamps null: false
-    end
-  end
+			t.timestamps null: false
+		end
+	end
 end
